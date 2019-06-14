a7cf871e1ffba2a1011b31085257aecd0a73be7d
turner11 <avi.turner111@gmail.com>
2019-06-14 16:52:50 +0300

    Introduced reactive extentions

---
+++ orchastrator.py
@@ -0,0 +1,99 @@
+from collections import namedtuple
+from functools import partial
+import cv2
+import rx
+from rx import operators as op
+import logging
+from imutils.video import FPS
+
+from trackers.opencv_tracker import OpenCvTracker
+from utils import init_log
+from yolo_object_detection.yolo import YoloDetector
+
+logger = logging.getLogger(__name__)
+KeyAndFrame = namedtuple('KeyAndFrame', ['key', 'frame'])
+
+
+class Orchstrator(object):
+    """"""
+
+    def __init__(self, url, yolo):
+        """"""
+        super().__init__()
+        self.url = url
+        self.yolo = yolo
+
+        self._should_track = False
+
+    def __repr__(self):
+        return super().__repr__()
+
+    @staticmethod
+    def get_stream(url, observer, scheduler):
+        cap = cv2.VideoCapture(url)
+        if not cap.isOpened():
+            observer.on_error("Error opening video stream or file")
+
+        fps = FPS().start()
+        try:
+            while cap.isOpened():
+                is_read_success, raw_frame = cap.read()
+                # Using the FPS for getting smooth video while waiting
+                fps.update()
+                fps.stop()
+                wait_time = max(fps.fps(), 1)
+                key = chr(cv2.waitKey(round(wait_time)) & 0xFF)
+                is_q_pressed = key == 'q'
+
+                if is_q_pressed:
+                    break
+
+                if is_read_success:
+                    observer.on_next(KeyAndFrame(key, raw_frame))
+                else:
+                    observer.on_error('Failed to read video capture')
+                    break
+        finally:
+            cap.release()
+        observer.on_completed()
+
+    def start(self):
+        url = self.url
+        source_func = partial(self.get_stream, url)
+        source = rx.create(source_func)
+
+        detector = YoloDetector.factory(yolo=self.yolo)
+        tracker = OpenCvTracker()
+
+        def track(key_and_frame):
+            should_reset = False
+            if key_and_frame.key == 's':
+                if self._should_track:
+                    should_reset = True
+
+                self._should_track = not self._should_track
+
+
+            if self._should_track:
+                is_success , frame = tracker.track(key_and_frame.frame)
+                should_reset = not is_success
+            else:
+                frame = key_and_frame.frame
+
+            if should_reset:
+                frame = key_and_frame.frame
+                self._should_track = False
+                tracker.reset()
+
+            return KeyAndFrame(key_and_frame.key, frame)
+
+        composed = source.pipe(
+            op.map(lambda kf: KeyAndFrame(kf.key, detector.detect(kf.frame))),
+            op.map(track),
+        )
+
+        composed.subscribe(on_next=lambda kf: cv2.imshow('Boxed Frames', kf.frame),
+                           on_completed=lambda: logger.debug("Stream ended"),
+                           on_error=lambda e: logger.exception('Got on error'))
+
+
---
+++ trackers/opencv_tracker.py
@@ -0,0 +1,82 @@
+import cv2
+from imutils.video import FPS
+
+OPENCV_OBJECT_TRACKERS = {
+        'csrt': cv2.TrackerCSRT_create,
+        'mosse': cv2.TrackerMOSSE_create,
+        'kcf': cv2.TrackerKCF_create,
+        'boosting': cv2.TrackerBoosting_create,
+        'mil': cv2.TrackerMIL_create,
+        'tld': cv2.TrackerTLD_create,
+        'medianflow': cv2.TrackerMedianFlow_create,
+    }
+
+
+class OpenCvTracker(object):
+    """"""
+
+    def __init__(self, tracker: str = 'csrt'):
+        """"""
+        super().__init__()
+        self.tracker = tracker
+        self.tracker_instance = self._get_tracker(tracker)
+        self.started_tracking = False
+        self.fps = None
+
+    def _get_tracker(self, tracker):
+        ctor = OPENCV_OBJECT_TRACKERS[tracker]
+        instance = ctor()
+        return instance
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(tracker={self.tracker})'
+
+    def track(self, frame) -> (object, bool):
+        tracker = self.tracker_instance
+
+        if not self.started_tracking:
+            init_bounding_box = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+            tracker.init(frame, init_bounding_box)
+            self.started_tracking = True
+            self.fps = FPS().start()
+
+        fps = self.fps
+
+        # grab the new bounding box coordinates of the object
+        (success, box) = tracker.update(frame)
+        # update the FPS counter
+        fps.update()
+        fps.stop()
+
+        # check to see if the tracking was a success
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+
+
+        # initialize the set of information we'll be displaying on
+        # the frame
+        info = [
+            ("Tracker", self.tracker),
+            ("Success", "Yes" if success else "No"),
+            ("FPS", "{:.2f}".format(fps.fps())),
+        ]
+
+        # output_frame = imutils.resize(frame, width=500)
+        (H, W) = frame.shape[:2]
+        # loop over the info tuples and draw them on our frame
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            color = (0, 255, 0) if success else (0, 0, 255)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+        return success, frame
+
+    def reset(self):
+        self.tracker_instance = self._get_tracker(self.tracker)
+        self.started_tracking = None
+        self.fps = None
+
+
