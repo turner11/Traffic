ee9bffa68b469011f1b3d63e3faf05c32afff0a6
turner11 <avi.turner111@gmail.com>
2019-06-15 23:41:46 +0300

    removed unused items

--- settings/user_settings.py
+++
@@ -1,4 +0,0 @@
-import os
-from pathlib import Path
-# This is a personal file for location of items. changes should not be committed.
-data_root_path = r'D:\cars_data'#str(Path(os.getcwd()).parent / 'cars_data')
