dacda48352c9ade0c182b54f509b3787f957a013
turner11 <avi.turner111@gmail.com>
2019-07-19 09:01:58 +0300

    Moved abstract builders to be in same file

--- builders/abstract_pipeline_builders.py
+++ builders/abstract_pipeline_builders.py
@@ -1,7 +1,21 @@
+from abc import ABC, abstractmethod
 from typing import List
-
-from builders.pipeline_builders import PipeLineBuilder
 from commands.abstract_command import FrameCommand
+
+
+class PipeLineBuilder(ABC):
+    """"""
+
+    def __init__(self):
+        """"""
+        super().__init__()
+
+    def __repr__(self):
+        return super().__repr__()
+
+    @abstractmethod
+    def get_commands(self) -> List[FrameCommand]:
+        pass
 
 
 class RunTimePipelineBuilder(PipeLineBuilder):
