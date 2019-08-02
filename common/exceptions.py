290b335a616b5caaafcf808a4478ca24387abf99
turner11 <avi.turner111@gmail.com>
2019-08-02 17:19:55 +0300

    Added exception type

--- common/exceptions.py
+++ common/exceptions.py
@@ -13,6 +13,12 @@ class ArgumentException(Exception):
         """"""
         super().__init__(message)
 
+class NoDataException(Exception):
+    def __init__(self, *args, **kwargs):
+        """"""
+        super().__init__(args, kwargs)
 
+    def __repr__(self):
+        return super().__repr__()
 
 
