15309280d14045609987e92f795af1f5456250b5
turner11 <avi.turner111@gmail.com>
2019-08-03 22:42:52 +0300

    Moved common augmentation commands to decorator

--- pipelines/_pipeline_operators.py
+++ pipelines/_pipeline_operators.py
@@ -14,6 +14,17 @@ from common.exceptions import ArgumentException
 from yolo_detectors.yolo_detector import YoloDetector
 
 
+def append_augmentation_commands(func):
+    def wrapper(*args, **kwargs):
+        augmentation_operators = [op.map(cmd) for cmd
+                                  in (DrawBoundingBoxCommand(), DisplayDebugCommand(), DrawStatsCommand())]
+        operators = func(*args, **kwargs)
+        return operators + augmentation_operators
+
+    return wrapper
+
+
+@append_augmentation_commands
 def _get_auto_track_operators(detector=None, tracker=None, **args):
     detector = detector or YoloDetector.factory(yolo=args.get('yolo'))
     tracker = tracker
@@ -23,27 +34,25 @@ def _get_auto_track_operators(detector=None, tracker=None, **args):
         # TabularDataCommand(),
         # MedianCommand()
     ]
-    augmentation_layers = [DrawBoundingBoxCommand(), DisplayDebugCommand(), DrawStatsCommand()]
-    commands = raw_data_layers + augmentation_layers + output_layers
+    commands = raw_data_layers + output_layers
     operators = [op.map(cmd) for cmd in commands]
 
     return operators
 
 
+@append_augmentation_commands
 def _get_debug_operators(detector=None, tracker=None, **args):
     detector = detector or YoloDetector.factory(yolo=args.get('yolo'))
     cmd_detect = DetectCommand(detector)
     cmd_track = TrackCommand(tracker)
     raw_data_layers = [cmd_detect, TrackDetectionsCommand(), ManualTrackingCommand(), cmd_track]
-    augmentation_layers = [DrawBoundingBoxCommand(), DisplayDebugCommand(), DrawStatsCommand()]
-    output_layers = []
 
-    commands = raw_data_layers + augmentation_layers + output_layers
+    commands = raw_data_layers
     operators = [op.map(cmd) for cmd in commands]
 
     return operators
 
-
+@append_augmentation_commands
 def _get_road_roi_detector_operators(detector=None, tracker=None, **args):
     affective_detector = detector or YoloDetector.factory(yolo=args.get('yolo'))
     if affective_detector is None:
@@ -57,13 +66,12 @@ def _get_road_roi_detector_operators(detector=None, tracker=None, **args):
     cmd_detect = DetectCommand(affective_detector, policy_controller=EveryNFramesPolicy(n=detection_n_frames))
 
     raw_data_layers = [cmd_detect]
-    data_aggregation_layers = [TabularDataCommand(policy_controller=EveryNFramesPolicy(n=detection_n_frames))]
+    data_aggregation_layers = [TabularDataCommand()]
     processing_layers = [
-         MedianCommand(policy_controller=EveryNFramesPolicy(n=median_n_frames))
+        MedianCommand(policy_controller=EveryNFramesPolicy(n=median_n_frames))
     ]
-    augmentation_layers = [DrawBoundingBoxCommand(), DisplayDebugCommand(), DrawStatsCommand()]
 
-    commands = raw_data_layers + data_aggregation_layers + processing_layers + augmentation_layers
+    commands = raw_data_layers + data_aggregation_layers + processing_layers
     operators = [op.map(cmd) for cmd in commands]
 
     return operators
