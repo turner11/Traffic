53cbd86679d14eb11875ebc379ee2c9a48026216
turner11 <avi.turner111@gmail.com>
2019-10-18 12:52:03 +0300

    Added option to save tabular data at end of session

--- observers/save_tabular_data_observer.py
+++ observers/save_tabular_data_observer.py
@@ -1,5 +1,8 @@
 import logging
 import shutil
+import sys
+
+import cv2
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -13,16 +16,15 @@ logger = logging.getLogger(__name__)
 
 class SaveTabularDataObserver(ObserverBase):
 
-    def __init__(self, path):
+    def __init__(self, folder, prompt=True):
         """"""
         super(SaveTabularDataObserver).__init__()
-        self.path = path
-        self.df = None
+        self.prompt = prompt
+        self.folder = Path(folder)
+        self.current_payload = None
 
     def on_next(self, payload: Payload):
-        df = payload.dfs.get('detections', pd.DataFrame()).drop_duplicates()
-        if len(df):
-            self.df = df
+        self.current_payload = payload
 
     def on_error(self, error):
         self.save_data()
@@ -33,7 +35,25 @@ class SaveTabularDataObserver(ObserverBase):
         super().on_completed()
 
     def save_data(self):
-        self.save_parquet(self.df, self.path, partition_col='label')
+        dfs = self.current_payload.session_data_frames
+        dfs = {name: df for name, df in dfs.items() if df is not None and len(df)}
+        should_save = len(dfs) and not self.prompt or self.should_save_payload()
+        if not should_save:
+            return
+
+        self.folder.mkdir(exist_ok=True, parents=True)
+
+        for name, df in dfs.items():
+            if len(df):
+                path = str(self.folder / name) + '.prqt'
+                partition_col = None  # 'label'
+                self.save_parquet(df, path, partition_col=partition_col)
+
+        for name, img in self.current_payload.images.items():
+            path = str(self.folder / name) + '.jpg'
+            cv2.imwrite(path, img)
+
+
 
     @staticmethod
     def save_parquet(df: pd.DataFrame, path: Union[str, Path], partition_col: Union[List[str], str, None] = None):
@@ -45,6 +65,7 @@ class SaveTabularDataObserver(ObserverBase):
             except Exception as ex:
                 logger.warning(f'Failed to delete parquet: {ex}')
 
+        # noinspection PyArgumentList
         table = pa.Table.from_pandas(df)
 
         if isinstance(partition_col, str):
@@ -62,3 +83,26 @@ class SaveTabularDataObserver(ObserverBase):
         #     raise NoDataException(str(e)) from e
 
         return parquet
+
+    def should_save_payload(self, default="yes"):
+        valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+        question = f'Should I save the data? ({str(self.folder)})'
+
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+
+        while True:
+            sys.stdout.write(f'{question} {prompt}')
+            choice = input().lower()
+            if default is not None and choice == '':
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes'/'no' (or 'y'/'n').\n")
