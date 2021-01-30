8fbd9fc3a1ba727cc7d452639c0b0d37e8cb3e2e
Avi Turner <avi.turner111@gmail.com>
2021-01-30 21:24:25 +0200

    Added scalfold for web display

---
+++ TrafficWeb/Traffic/Traffic/Shared/CascadingAppState.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrafficService;
+
+namespace Traffic.Shared
+{
+    public partial class CascadingAppState
+    {
+        [Inject]
+        ITrafficBackend trafficBackend { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        Camera _camera;
+        public Camera Camera
+        {
+            get => this._camera; 
+            internal set
+            {
+                this._camera = value;
+                this.StateHasChanged();
+            }
+        }
+
+        internal async Task<List<Camera>> GetCameras()
+        {
+            return await this.trafficBackend.GetCameras();
+        }
+    }
+}
